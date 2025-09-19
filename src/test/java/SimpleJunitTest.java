5cac28517bccecadabce6a629771dea45812daa2
Vaqif Yusibli <vaqifyusibli@gmail.com>
2025-09-20 00:05:43 +0200

    Fix .gitignore, remove unnecessary files from Git

--- src/test/java/SimpleJunitTest.java
+++ src/test/java/SimpleJunitTest.java
@@ -1,2 +1,18 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class SimpleJunitTest {
+
+    @Test
+    void test() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        open("https://duckduckgo.com/");
+        $("[name=q]").setValue("милена").pressEnter();
+        $(".react-results--main").shouldHave(text("Текст"));
+    }
 }
